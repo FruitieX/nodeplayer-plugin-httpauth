6c85b9c92a1c5539501f0c08b79aec7ce613711b
FruitieX <fruitiex@gmail.com>
2015-04-18 21:55:19 +0300

    fix indent

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ exports.init = function(player, logger, callback) {
         callback('module must be initialized after express module!');
     } else {
         var basic = auth.basic({
-                realm: 'nodeplayer'
-            }, function(username, password, callback) {
-                callback(username === config.username && password === config.password);
-            });
+            realm: 'nodeplayer'
+        }, function(username, password, callback) {
+            callback(username === config.username && password === config.password);
+        });
 
         _.each(config.paths, function(path) {
+            logger.debug('password protecting path: ' + path);
             player.app.use(path, auth.connect(basic));
         });
         callback();
